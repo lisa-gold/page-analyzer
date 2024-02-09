e08a56f89a28c4dd0df094d40283500a2b96ac62
Elizaveta <ukrop@LAPTOP-T43LJSG0>
2024-02-09 22:41:19 +0200

    minor corrections in app.py

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -141,12 +141,8 @@ def get_information(response):
     html = BeautifulSoup(response.text, 'lxml')
     status_code = response.status_code
 
-    try:
-        h1 = html.h1.text
-        title = html.title.text
-    except BaseException:
-        h1 = ''
-        title = ''
+    h1 = html.h1.text if html.find('h1') else ''
+    title = html.title.text if html.find('title') else ''
 
     description = ''
     metas = html.head.find_all('meta')
